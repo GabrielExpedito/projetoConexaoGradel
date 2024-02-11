e26514a6c07c70e90629b231a5adf0e7a439dbff
Gabriel Expedito <gabrielexpedi18@gmail.com>
2024-02-11 13:16:56 -0300

    "Terceiro commit, elaborado o método para select"

--- projetoGradle/app/src/main/java/projetoGradle/App.java
+++ projetoGradle/app/src/main/java/projetoGradle/App.java
@@ -1,27 +1,62 @@
 package projetoGradle;
 
 import bancolib.SqlUtil;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
 
 public class App {
 
     public static void main(String[] args) {
+        SelectBanco();
+    }
+    
+    private static void InsertBanco() {
+        
+    }
+
+    private static void SelectBanco() {
+        
+        
         try {
-        SqlUtil sqlUtil
-                = new SqlUtil("localhost", "5432", "Lista 2",
-                "postgres", "12345");
-        
-        Statement stm = sqlUtil.getConn().createStatement();
-        StringBuilder comando = new StringBuilder();
-        
-        comando.append("SELECT * from produtos"); 
-        
-        //stm.execute(sql) //Alterações no banco de dados
-        //stm.executeQuery(sql) //Retorno de Dados do banco
-                
-        ResultSet resultSet = stm.executeQuery(comando.toString()); //Trás basicamente um array de 24 posições, aonde cada posição é uma linha
+            SqlUtil sqlUtil
+                    = new SqlUtil("localhost", "5432", "Teste",
+                            "postgres", "12345");
+
+            Statement stm = sqlUtil.getConn().createStatement();
+            StringBuilder comando = new StringBuilder();
+
+            comando.append("\nSELECT * from produtos");
+            ResultSet resultSet = stm.executeQuery(comando.toString());
+
+            while (resultSet.next()) {
+                int id_nf = resultSet.getInt("id_nf");
+                int id_item = resultSet.getInt("id_item");
+                int cod_prod = resultSet.getInt("cod_prod");
+                double valor_unit = resultSet.getDouble("valor_unit");
+                double quantidade = resultSet.getDouble("quantidade");
+                double desconto = resultSet.getDouble("desconto");
+
+                comando.append("\nTabela Produtos: ").append("\nid_nf: ").append(id_nf).append("\nid_item: ").append(id_item).append("\ncod_prod: ").append(cod_prod).append("\nvalor_unit: ")
+                        .append(valor_unit)
+                        .append("\nquantidade: ").append(quantidade).append("\ndesconto: ").append(desconto);
+
+                System.out.println(comando.toString());
+
+            }
+
+        } catch (Exception ex) {
+            System.out.println("Algo de errado não está certo");
+        }
+    }
+    }
+
+
+
+//stm.execute(sql) //Alterações no banco de dados
+//stm.executeQuery(sql) //Retorno de Dados do banco
+
+        /*ResultSet resultSet = stm.executeQuery(comando.toString()); //Trás basicamente um array de 24 posições, aonde cada posição é uma linha
         
         while (resultSet.next()) {
             System.out.println(resultSet.getInt("cod_prod"));
@@ -45,11 +80,4 @@ public class App {
                 //Realizar apenas os cadastros não fazer a parte do estacionamento.
                 //Ex: Cadastro de clientes Solicitar os dados da tabel (CPF, nome e data nascimento), ao terminar
                 // terminar de solicitar os dados, confirmar os dados para o usuário. Se correto os dados deverão ser salvos no banco de dados.
-        comandoInsert.append(")");
-                 
-            
-        } catch (Exception ex) {
-            System.out.println("Algo de errado não está certo");
-        }
-    }
-}
+        comandoInsert.append(")");*/
