7206da7db349a8862808ae3666cf70f3ea19d419
Gabriel Expedito <gabrielexpedi18@gmail.com>
2024-02-13 13:16:50 -0300

    Quarto commit: Incluído Método InsertBanco que cadastra o cliente no banco Incluído Método selectBanco que puxa todos os cadastros realizados no banco !#	modified:   projetoGradle/.idea/compiler.xml

--- projetoGradle/app/src/main/java/projetoGradle/App.java
+++ projetoGradle/app/src/main/java/projetoGradle/App.java
@@ -1,23 +1,93 @@
 package projetoGradle;
 
 import bancolib.SqlUtil;
+
+import javax.swing.*;
+import java.nio.charset.Charset;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Scanner;
 
 public class App {
 
+        public static final int CADASTRAR_CLIENTE = 1;
+        public static final int VERIFICAR_CLIENTES = 2;
+        public static final int SAIR = 0;
+
     public static void main(String[] args) {
-        SelectBanco();
+        Scanner tecladoPrincipal = new Scanner(System.in);
+        int escolha = 0;
+
+        do {
+            System.out.println("---------- MENU ESTACIONAMENTO ---------");
+            System.out.println("1-) Cadastrar Novo Cliente");
+            System.out.println("2-) Verificar clientes cadastrados");
+            System.out.println("0-) Sair do Menu");
+            escolha = tecladoPrincipal.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    InsertBanco();
+                    break;
+                case 2:
+                    SelectBanco();
+                    break;
+                case 0:
+                    break;
+            }
+        } while (escolha != 0);
+        System.out.println("Finalizando o programa");
     }
-    
-    private static void InsertBanco() {
-        
+
+    public static void InsertBanco() {
+        try {
+            SqlUtil sqlUtil
+                    = new SqlUtil("localhost", "5432", "Teste",
+                            "postgres", "12345");
+        Cliente cli = new Cliente();
+        String opcao = "";
+
+        Scanner tecladoPrincipal = new Scanner(System.in);
+
+        System.out.println("Insira dos dados do cliente que deseja cadastrar: ");
+        System.out.println("CPF: ");
+        cli.setCpf(tecladoPrincipal.nextLine());
+        System.out.println("Nome: ");
+        cli.setNome(tecladoPrincipal.nextLine());
+        System.out.println("Data de Nascimento: ");
+        cli.setDtNasc(tecladoPrincipal.nextLine());
+
+        System.out.println("Confirme os dados que quer cadastrar: ");
+        System.out.println("CPF: " + cli.getCpf());
+        System.out.println("Nome: " + cli.getNome());
+        System.out.println("Data de Nascimento: " + cli.getDtNasc());
+        System.out.println("Digite Y para sim e N para não: ");
+        opcao = tecladoPrincipal.nextLine();
+
+        if (opcao.equals("Y")) {
+            System.out.println("Os dados serão inseridos no banco");
+
+
+            String comandoInsert = "insert into cliente (cpf, nome, dtNasc) values (?, ?, ?)";
+            PreparedStatement pst = sqlUtil.getConn().prepareStatement(comandoInsert);
+            pst.setString(1, cli.getCpf());
+            pst.setString(2, cli.getNome());
+            pst.setString(3, cli.getDtNasc());
+            pst.executeUpdate();
+            System.out.println("Dados inseridos com sucesso");
+
+        } else {
+            System.out.println("Não será inserido os dados desse cliente no banco");
+        }
+        } catch (Exception ex) {
+            System.out.println(ex.getLocalizedMessage());
+        }
     }
 
     private static void SelectBanco() {
-        
-        
+
         try {
             SqlUtil sqlUtil
                     = new SqlUtil("localhost", "5432", "Teste",
@@ -26,20 +96,15 @@ public class App {
             Statement stm = sqlUtil.getConn().createStatement();
             StringBuilder comando = new StringBuilder();
 
-            comando.append("\nSELECT * from produtos");
+            comando.append("\nSELECT * from cliente");
             ResultSet resultSet = stm.executeQuery(comando.toString());
 
             while (resultSet.next()) {
-                int id_nf = resultSet.getInt("id_nf");
-                int id_item = resultSet.getInt("id_item");
-                int cod_prod = resultSet.getInt("cod_prod");
-                double valor_unit = resultSet.getDouble("valor_unit");
-                double quantidade = resultSet.getDouble("quantidade");
-                double desconto = resultSet.getDouble("desconto");
+                String cpf = resultSet.getString("cpf");
+                String nome = resultSet.getString("nome");
+                String dtNasc = resultSet.getString("dtNasc");
 
-                comando.append("\nTabela Produtos: ").append("\nid_nf: ").append(id_nf).append("\nid_item: ").append(id_item).append("\ncod_prod: ").append(cod_prod).append("\nvalor_unit: ")
-                        .append(valor_unit)
-                        .append("\nquantidade: ").append(quantidade).append("\ndesconto: ").append(desconto);
+                comando.append("\nTabela Cliente: ").append("\nCPF: ").append(cpf).append("\nNome: ").append(nome).append("\nData Nasc: ").append(dtNasc);
 
                 System.out.println(comando.toString());
 
@@ -49,14 +114,12 @@ public class App {
             System.out.println("Algo de errado não está certo");
         }
     }
-    }
-
-
+}
 
 //stm.execute(sql) //Alterações no banco de dados
 //stm.executeQuery(sql) //Retorno de Dados do banco
 
-        /*ResultSet resultSet = stm.executeQuery(comando.toString()); //Trás basicamente um array de 24 posições, aonde cada posição é uma linha
+/*ResultSet resultSet = stm.executeQuery(comando.toString()); //Trás basicamente um array de 24 posições, aonde cada posição é uma linha
         
         while (resultSet.next()) {
             System.out.println(resultSet.getInt("cod_prod"));
